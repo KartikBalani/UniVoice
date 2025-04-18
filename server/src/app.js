20f924b1b70aaa4daceaeab5d88741da441a29dd
KartikBalani <144781496+KartikBalani@users.noreply.github.com>
2025-04-18 15:12:18 +0530

    Update app.js

--- server/src/app.js
+++ server/src/app.js
@@ -24,7 +24,7 @@ import navbarUpdate from "./routes/navbarUpdate.js";
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT | 3000;
 const murl = process.env.Mongo_URI;
 
 
